227a6c11ed944ad28671a097ba648663baba2a00
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-08-13 20:13:41 +0500

    Potfolio using react.js

--- portfolio-project/src/Components/Header.jsx
+++ portfolio-project/src/Components/Header.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-      
+    <div className='header'>
+      <div><h1>MUMTAZ WALI</h1></div>
+      <nav>
+        <ul className="nav-links">
+          <li><a href="">Home</a></li>
+          <li><a href="">About Me</a></li>
+          <li><a href=""></a>Projects</li>
+          <li><a href="">Contacts</a></li>
+          <li><a href=""></a></li>
+        </ul>
+      </nav>
     </div>
   )
 }
