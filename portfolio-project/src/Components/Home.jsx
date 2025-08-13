ee8f2dd89d06db3b88643e587fb02e9122cff7a8
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-08-13 21:58:53 +0500

    Potfolio using react.js

--- portfolio-project/src/Components/Home.jsx
+++ portfolio-project/src/Components/Home.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import "../Components/Home.css"
 function Home() {
   return (
-    <div>
-      
+    <div className='Hero-section'>
+        
+            <div className='Hero'>
+                <h3>Hello <span>.</span></h3>
+                <h4>I'm Mumtaz Wali</h4>
+                <h2>Front-End Developer</h2>
+            </div>
+            <div className="Hero-button">
+               <button><a href="">My resume</a></button> 
+            </div>
+            <div className='Hero-img'>
+        </div>
     </div>
   )
 }
