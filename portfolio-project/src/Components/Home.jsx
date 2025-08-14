7d3e83a93b38d28404984d56d070cac4adb434ed
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-08-14 18:51:34 +0500

    Potfolio using react.js

--- portfolio-project/src/Components/Home.jsx
+++ portfolio-project/src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../Components/Home.css"
+import heroImage from "./images/img1.png"
 function Home() {
   return (
     <div className='Hero-section'>
@@ -8,14 +9,18 @@ function Home() {
                 <h3>Hello <span>.</span></h3>
                 <h4>I'm Mumtaz Wali</h4>
                 <h2>Front-End Developer</h2>
+               <button><a href="">My resume</a></button>
+
             </div>
-            <div className="Hero-button">
-               <button><a href="">My resume</a></button> 
-            </div>
+            {/* <div className="Hero-button">
+            </div> */}
             <div className='Hero-img'>
+             <img src={heroImage} alt="Mumtaz Wali" />
         </div>
     </div>
   )
 }
 
 export default Home
+
+
