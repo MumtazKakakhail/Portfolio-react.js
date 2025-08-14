21e41d6ac474e2ad393179de8c5efc8401b1d433
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-08-14 20:57:26 +0500

    making home section

--- portfolio-project/src/Components/Home.jsx
+++ portfolio-project/src/Components/Home.jsx
@@ -4,18 +4,18 @@ import heroImage from "./images/img1.png"
 function Home() {
   return (
     <div className='Hero-section'>
-        
-            <div className='Hero'>
-                  <h3>Hello <span>.</span></h3>
-                <h4>I'm Mumtaz Wali</h4>
-                <h2>Front-End Developer</h2>
-               <button><a href="">My resume</a></button>
-            </div>
-            {/* <div className="Hero-button">
+
+      <div className='Hero'>
+        <h3>Hello <span>.</span></h3>
+        <h4>I'm Mumtaz Wali</h4>
+        <h2>Front-End Developer</h2>
+        <button><a href="">My resume</a></button>
+      </div>
+      {/* <div className="Hero-button">
             </div> */}
-            <div className='Hero-img'>
-             <img src={heroImage} alt="Mumtaz Wali" />
-        </div>
+      <div className='Hero-img'>
+        <img src={heroImage} alt="Mumtaz Wali" />
+      </div>
     </div>
   )
 }
