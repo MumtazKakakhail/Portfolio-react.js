2e81a3c44ed63d29426c7bf41c5c75c40a30a0c9
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-08-18 18:34:38 +0500

    AboutMe Section

--- portfolio-project/src/Components/AboutMe.jsx
+++ portfolio-project/src/Components/AboutMe.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import "../Components/AboutMe.css"
+import React from "react";
+import "../Components/AboutMe.css";
 function AboutMe() {
   return (
-    <div>
-      
+    <div className="About">
+      <div className="AboutMe">
+        <h2>About Me</h2>
+        <p>
+          I'm a passionate frontend developer with expertise in creating modern
+          web applications. My focus is on building responsive, accessible, and
+          performant user interfaces that provide exceptional user experiences.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AboutMe
+export default AboutMe;
