d69e4da4aab091df9f557fce0e7c858308fff037
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-08-18 22:29:09 +0500

    adding animations using framer motion

--- portfolio-project/src/Components/Home.jsx
+++ portfolio-project/src/Components/Home.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import "../Components/Home.css"
 import heroImage from "./images/img1.png"
+// eslint-disable-next-line no-unused-vars
+import { motion } from "motion/react"
+import { Typewriter } from './Typewriter'
 function Home() {
   return (
     <div id='home' className='Hero-section'>
 
       <div className='Hero'>
-        <h3>Hello <span>.</span></h3>
-        <h4>I'm Mumtaz Wali</h4>
-        <h2>Front-End Developer</h2>
+        <Typewriter text={"Hello"} />
+        <Typewriter text={"I'm Mumtaz Wali"} />
+        <Typewriter text={"Frontend Developer"} />
         <button><a href="">My resume</a></button>
       </div>
       {/* <div className="Hero-button">
             </div> */}
       <div className='Hero-img'>
-        <img src={heroImage} alt="Mumtaz Wali" />
+        <img src={heroImage} alt="Mumtaz Wali"/>
       </div>
     </div>
   )
