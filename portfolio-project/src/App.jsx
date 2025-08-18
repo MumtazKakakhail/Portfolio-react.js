6bf43ee2ded8db7400ad781bd15304fad4b76141
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-08-18 18:31:26 +0500

    AboutMe Section

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Components/Header'
 import Home from './Components/Home'
+import AboutMe from './Components/AboutMe'
 
 function App() {
   return (
     <div>
       <Header/>
       <Home/>
+      <AboutMe/>
     </div>
   )
 }
