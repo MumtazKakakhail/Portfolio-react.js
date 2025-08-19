5cd709e41aa8de338d51aa783ede52115c2c9845
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-08-19 19:28:04 +0500

    adding animations using framer motion

--- portfolio-project/src/Components/Home.jsx
+++ portfolio-project/src/Components/Home.jsx
@@ -2,25 +2,69 @@ import React from 'react'
 import "../Components/Home.css"
 import heroImage from "./images/img1.png"
 // eslint-disable-next-line no-unused-vars
-import { motion } from "motion/react"
+import { motion } from "framer-motion"
 import { Typewriter } from './Typewriter'
 function Home() {
   return (
-    <div id='home' className='Hero-section'>
+//     <div id='home' className='Hero-section'>
 
+//       <div className='Hero'>
+//         <Typewriter text={"Hello"} />
+//         <Typewriter text={"I'm Mumtaz Wali"} />
+//         <Typewriter text={"Frontend Developer"} />
+//         <button><a href="">My resume</a></button>
+//       </div>
+//       {/* <div className="Hero-button">
+//             </div> */}
+//       <div className='Hero-img'>
+//         <img src={heroImage} alt="Mumtaz Wali"/>
+//       </div>
+//     </div>
+ <div id='home' className='Hero-section'>
       <div className='Hero'>
-        <Typewriter text={"Hello"} />
-        <Typewriter text={"I'm Mumtaz Wali"} />
-        <Typewriter text={"Frontend Developer"} />
-        <button><a href="">My resume</a></button>
+        <motion.div
+          initial={{ opacity: 0, y: -30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <Typewriter text={"Hello"} />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: -30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.2, delay: 0.3 }}
+        >
+          <Typewriter text={"I'm Mumtaz Wali"} />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: -30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.2, delay: 0.6 }}
+        >
+          <Typewriter text={"Frontend Developer"} />
+        </motion.div>
+        <motion.button
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 1 }}
+          className="resume-btn"
+        >
+          <a href="">My resume</a>
+        </motion.button>
       </div>
-      {/* <div className="Hero-button">
-            </div> */}
       <div className='Hero-img'>
-        <img src={heroImage} alt="Mumtaz Wali"/>
+        <motion.img
+          src={heroImage}
+          alt="Mumtaz Wali"
+          initial={{ opacity: 0, scale: 0.9, x: 50 }}
+          animate={{ opacity: 1, scale: 1, x: 0 }}
+          transition={{ duration: 1, delay: 1.2 }}
+        />
       </div>
     </div>
   )
 }
+
+
 
 export default Home;
