d7df188c5f0d9e71e5b121bf546854487956de75
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-08-21 21:56:27 +0500

    updating header section

--- portfolio-project/src/Components/Header.jsx
+++ portfolio-project/src/Components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="">Projects</a></li>
-          <li><a href="#contact">Contacts</a></li>
+          <li><a href="#contact">Contact</a></li>
           <li><a href=""></a></li>
         </ul>
       </nav>
