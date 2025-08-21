1fc56d8f7135432c0a5ee53ee42f08fa6b24b444
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-08-21 21:49:07 +0500

    Adding footer section

--- portfolio-project/src/Components/Footer.jsx
+++ portfolio-project/src/Components/Footer.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import "./Footer.css"
+import { TiSocialLinkedin } from "react-icons/ti";
+import { FaGithub } from "react-icons/fa";
+import { SiUpwork } from "react-icons/si";
+import { SiFiverr } from "react-icons/si";
 function Footer() {
     return (
         <div className='footer'>
             <div className="container">
-                <h2>Mumtaz Wali</h2>
-                <p>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</p>
+                <h2 className='heading'>MUMTAZ WALI</h2>
+                <p>A Frontend focused Web Developer building the Frontend of Websites and Web Applications <br />
+                 that leads to the success of the overall product.</p>
+            </div>
+            <div className="socials">
+                <h2 className='heading'>SOCIAL</h2>
+              <a href=""><TiSocialLinkedin /></a> 
+               <a href=""><FaGithub /></a> 
+               <a href=""><SiUpwork /></a> 
+               <a href=""><SiFiverr /></a> 
             </div>
         </div>
     )
