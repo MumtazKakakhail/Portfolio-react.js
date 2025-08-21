dd335d2e0e4e7fae367cb4c8e770afa26eeb54a8
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-08-21 19:38:26 +0500

    updating contact section

--- portfolio-project/src/Components/Contact.jsx
+++ portfolio-project/src/Components/Contact.jsx
@@ -32,6 +32,7 @@ function Contact() {
       <div className="contact-form">
         <div className="touch">
           <h2>Get In Touch</h2>
+          <p>Feel free to Contact me by submitting the form below and I will get back to you as soon as possible</p>
         </div>
         <form action="#" method="POST">
           <div className="form-group">
