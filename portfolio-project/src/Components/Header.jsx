93d2bab4e813f1bb87adda8093fd6f8febd84ea7
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-08-26 19:01:28 +0500

    making portfolio responsive

--- portfolio-project/src/Components/Header.jsx
+++ portfolio-project/src/Components/Header.jsx
@@ -1,19 +1,33 @@
-import "./Header.css"
+import React, { useState } from "react";
+import "./Header.css";
+import { HiMenu } from "react-icons/hi";
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="header">
-      <div><h1>MUMTAZ WALI</h1></div>
+      <div>
+        <h1>MUMTAZ WALI</h1>
+      </div>
       <nav>
-        <ul className="nav-links">
+        <div
+          className={`menu-toggle${menuOpen ? " open" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+        <a href="" className="menu"> <HiMenu /></a> 
+          {/* <span />
+          <span />
+          <span /> */}
+        </div>
+        <ul className={`nav-links${menuOpen ? " show" : ""}`}>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
-          <li><a href=""></a></li>
         </ul>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
